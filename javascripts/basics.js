3272c860d7708849529f9b8e7a3b2f2173cbc569
aiasfina <aiasfina@gmail.com>
2014-10-11 19:50:42 +0800

    scroll top after render

--- javascripts/basics.js
+++ javascripts/basics.js
@@ -4,5 +4,18 @@ jQuery(function() {
     return $(ele).html(prettyDate(new Date($(this).data("time"))));
   });
 
-  $('#main-content').smoothState();
+
+  var $body = $("html, body");
+  $('#main-content').smoothState({
+      onEnd: {
+          render: function(url, $container, $content) {
+              $body.css('cursor', 'auto');
+              $body.find('a').css('cursor', 'auto');
+              $container.html($content);
+              setTimeout(function() {
+                  $body.animate({scrollTop: 0}, 500);
+              }, 0);
+          }
+      }
+  });
 });
